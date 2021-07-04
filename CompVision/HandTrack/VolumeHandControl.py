6521983aeb567c0707255e4593f412c402a64842
Nickolazz1 <elanir358@gmail.com>
2021-07-04 15:50:04 +0300

    volume control in progress

--- CompVision/HandTrack/VolumeHandControl.py
+++ CompVision/HandTrack/VolumeHandControl.py
@@ -1,6 +1,9 @@
 import cv2
 import time
 import numpy as np
+import HandTrackingModule as htm
+import math
+
 
 ####################################
 wCam, hCam = 640, 480   # not worked
@@ -11,11 +14,29 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
+detector = htm.HandDetector(detectionCon=0.7)
 
 while True:
     success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        #print(lmList[4], lmList[8])
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1+x2)//2, (y1+y2)//2
 
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
+        length = math.hypot(x2 - x1, y2 - y1)
+        #print(length)
+        if length<50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+        # timeline: 3:12:41 ...
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
